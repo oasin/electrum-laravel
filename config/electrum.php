60f7c65d7b0a306e4025a8ecfcfafeae41453159
oasin <31675779+oasin@users.noreply.github.com>
2022-05-06 20:15:30 +0100

    PhpStorm Commit

--- config/electrum.php
+++
@@ -1,5 +0,0 @@
-<?php
-// config for Oasin/ClassName
-return [
-
-];
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Oasin\Electrum\Database\Factories;
+namespace Oasin\Electrumvel\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
